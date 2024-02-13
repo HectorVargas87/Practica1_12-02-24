c65a10e41baf130146ef13b8e1e8262f08f11739
HectorVargas87 <113728891+HectorVargas87@users.noreply.github.com>
2024-02-12 22:17:56 -0600

    Add files via upload
    Actualizacion _ agregar "get por id"

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const port = 3800;
 
@@ -9,35 +10,42 @@ let alumnos =[
     {id:3,Nombre: 'Tocayo' }
 ]
 
-
-app.get('/', (req,res)=>{
-    res.send('hola a todos')
-})
 app.get("/alumnos",(req,res) => {
     res.send(alumnos);
 });
-app.post("/alumnos ", (req,res)=>{
+app.post("/alumnos", (req,res)=>{
     const {body} = req
+    console.log(body)
     alumnos.push(body);
     res.send("Alumno agregado satisfactoriamente");
 });
-app.patch("/alumnos", (req,res) => {
+app.patch("/alumnos/:id", (req,res) => {
     const {body} = req
-    const {id} = req
-    const {nombre} = req
+    const {id} = req.params
+    const {Nombre} = body
 
-    let alumnos = alumnos.find((alumnos) => alumnos.id == id)
-    alumnos.Nombre = Nombre
-    res.send({message: "Se quito alumno"})
+    let alumno = alumnos.find((a) => a.id == id)
+    alumno.Nombre = Nombre
+    res.send({message: "Se actualizo alumno"})
 });
 app.delete("/alumnos/:id",(req,res) => {
     const {id} = req.params;
-    alumnos= alumnos.filter((alumnos) => alumnos.id != id );
+    alumnos = alumnos.filter((alumnos) => alumnos.id != id );
     res.send({message:"Alumno eliminado"})
 });
-app.listen(port , ()=>{console.log('Server is running on ${port}')});
+app.listen(port , ()=>{console.log(`Server is running on ${port}`)});
 app.get('/', (req,res) => {
     res.send("Hola a todos!!!")
 })
 
 /*Get por ID*/
+app.get("/alumnos/:id", (req, res) => {
+    const { id } = req.params;
+    const alumno = alumnos.find((a) => a.id == id);
+
+    if (alumno) {
+        res.send(alumno);
+    } else {
+        res.status(404).send({ message: "Alumno no encontrado" });
+    }
+});
